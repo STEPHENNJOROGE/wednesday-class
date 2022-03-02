f5530448090509b91da54aeb35fafaccb16d212e
STEPHENNJOROGE <99167029+STEPHENNJOROGE@users.noreply.github.com>
2022-03-02 15:40:22 +0300

    Add files via upload

---
+++ code.py
@@ -0,0 +1,20 @@
+Dict = {}
+print(Dict)
+
+Dict = dict({1: 'Geeks',2: 'For', 3:'geeks'})
+print(Dict)
+
+Dict = dict([(1,'geeks'),(2,'For')])
+print(Dict)
+
+
+Dict = {}
+print("empty dictionary")
+print(Dict)
+
+
+Dict[0]='Geeks'
+Dict[2]='For'
+Dict[3]=1
+print("\ndictionary after adding 3 elements")
+print(Dict)
